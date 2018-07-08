9945e1ddf3eefe9dde723f6af9c1f35af14d133c
earthmant <trammell@cloudify.co>
2018-07-08 18:35:36 +0300

    add teardown

--- ecosystem_tests/__init__.py
+++ ecosystem_tests/__init__.py
@@ -1,12 +1,13 @@
 
 # Standard Imports
 import os
-import sys
 import re
+import sys
+import shutil
 import tempfile
 
 # Third Parties Imports
-import testtools
+import unittest
 from cloudify.workflows.local import init_env, load_env, FileStorage
 from cloudify.test_utils.local_workflow_decorator \
     import IGNORED_LOCAL_WORKFLOW_MODULES
@@ -31,6 +32,7 @@ DIAMOND_WAGON = 'https://github.com/cloudify-cosmo/' \
 DIAMOND_YAML = 'https://github.com/cloudify-cosmo/' \
                'cloudify-diamond-plugin/releases/' \
                'download/1.3.8/plugin.yaml'
+CFY_LOCAL_FILE = 'test'
 
 
 class PasswordFilter(object):
@@ -58,30 +60,53 @@ class PasswordFilter(object):
         self.stream.flush()
 
 
-class EcosystemTestBase(testtools.TestCase):
+class EcosystemTestBase(unittest.TestCase):
 
-    if 'ECOSYSTEM_SESSION_PASSWORD' not in os.environ:
-        os.environ['ECOSYSTEM_SESSION_PASSWORD'] = create_password()
-    password = os.environ['ECOSYSTEM_SESSION_PASSWORD']
+    @classmethod
+    def setUpClass(cls):
+        if 'ECOSYSTEM_SESSION_PASSWORD' not in os.environ:
+            os.environ['ECOSYSTEM_SESSION_PASSWORD'] = create_password()
+
+    @classmethod
+    def tearDownClass(cls):
+        cfy_storage = FileStorage(storage_dir=CFY_LOCAL_FILE)
+        cfy_local = cls.load_cfy_local(cfy_storage)
+        cls.uninstall_manager(cfy_local)
+        shutil.rmtree(CFY_LOCAL_FILE)
+        try:
+            os.environ['ECOSYSTEM_SESSION_MANAGER_IP']
+        except KeyError:
+            pass
 
     def setUp(self):
+        super(EcosystemTestBase, self).setUp()
         if self.password not in self.sensitive_data:
             self.sensitive_data.append(self.password)
         sys.stdout = PasswordFilter(self.sensitive_data, sys.stdout)
         sys.stderr = PasswordFilter(self.sensitive_data, sys.stderr)
-        super(EcosystemTestBase, self).setUp()
+        self.cfy_local = self.setup_cfy_local()
         if 'ECOSYSTEM_SESSION_MANAGER_IP' in os.environ:
             self.manager_ip = \
                 os.environ['ECOSYSTEM_SESSION_MANAGER_IP']
         else:
-            self.cfy_local = self.setup_cfy_local()
             self.install_manager()
             self.initialize_manager_profile()
             self.upload_plugins()
 
-    def tearDown(self):
-        super(EcosystemTestBase, self).tearDown()
-        self.uninstall_manager()
+    @staticmethod
+    def load_cfy_local(_storage):
+        return load_env(name='local', storage=_storage)
+
+    @staticmethod
+    def uninstall_manager(cfy_local):
+        cfy_local.execute(
+            'uninstall',
+            task_retry_interval=15,
+            parameters={'ignore_failure': 'true'})
+
+    @property
+    def password(self):
+        return os.environ['ECOSYSTEM_SESSION_PASSWORD']
 
     @property
     def plugins_to_upload(self):
@@ -168,7 +193,7 @@ class EcosystemTestBase(testtools.TestCase):
         return os.environ['ECOSYSTEM_SESSION_BLUEPRINT_DIR']
 
     @property
-    def test_application_prefix(self):
+    def application_prefix(self):
         """some prefix for appending to resource names"""
         if 'CIRCLE_BUILD_NUM' in os.environ:
             return os.environ['CIRCLE_BUILD_NUM']
@@ -193,7 +218,7 @@ class EcosystemTestBase(testtools.TestCase):
         :rtype: cloudify.workflows.local._Environment
         """
 
-        cfy_storage = FileStorage(storage_dir='test')
+        cfy_storage = FileStorage(storage_dir=CFY_LOCAL_FILE)
         if not os.environ.get('ECOSYSTEM_SESSION_LOADED', False):
             os.environ['ECOSYSTEM_SESSION_LOADED'] = 'true'
             return init_env(
@@ -204,7 +229,7 @@ class EcosystemTestBase(testtools.TestCase):
                 inputs=self.inputs,
                 storage=cfy_storage,
                 ignored_modules=IGNORED_LOCAL_WORKFLOW_MODULES)
-        return load_env(name='local', storage=cfy_storage)
+        return self.load_cfy_local(cfy_storage)
 
     def install_manager(self):
         """install a cloudify manager using local profile
@@ -213,55 +238,12 @@ class EcosystemTestBase(testtools.TestCase):
         :rtype: NoneType
         """
 
-        self.addCleanup(self.uninstall_manager)
-        self.cfy_local.execute(
-            'install',
-            task_retries=45,
-            task_retry_interval=10)
+        self.cfy_local.execute('install', task_retry_interval=15)
         self.node_instances = \
             self.cfy_local.storage.get_node_instances()
-        self.manager_ip = self.get_manager_ip()
-        os.environ['BOOTSTRAPPED'] = 'true'
-        self.check_manager_resources()
-
-    def uninstall_manager(self):
-        """uninstall a cloudify manager using local profile
-
-        :returns: nothing
-        :rtype: NoneType
-        """
-
-        if 'BOOTSTRAPPED' in os.environ:
-            initialize_cfy_profile()
-            self.cfy_local.execute(
-                'uninstall',
-                task_retries=45,
-                task_retry_interval=10)
-
-    def check_manager_resources(self,
-                                node_type_prefix=None,
-                                id_property=None,
-                                check_resource_method=None):
-        """get all resources and check they exist
-
-        :param node_type_prefix: the node types of the plugin to check
-        :param id_property: the property for resource ID
-        :param check_resource_method: the method that checks resources
-        :returns: nothing
-        :rtype: NoneType
-        """
-
-        node_type_prefix = node_type_prefix or self.node_type_prefix
-        id_property = id_property or self.server_ip_property
-        check_resource_method = \
-            check_resource_method or self.check_resource_method
-
-        for resource in get_resource_ids_by_type(
-                self.node_instances,
-                node_type_prefix,
-                self.cfy_local.storage.get_node,
-                id_property=id_property):
-            check_resource_method(resource_id=resource)
+        ip = self.get_manager_ip()
+        os.environ['ECOSYSTEM_SESSION_MANAGER_IP'] = ip
+        self.manager_ip = ip
 
     def upload_plugins(self, plugin_mapping=None):
         """upload plugins after manager install
@@ -274,7 +256,7 @@ class EcosystemTestBase(testtools.TestCase):
         plugin_mapping = plugin_mapping or self.plugin_mapping
 
         update_plugin_yaml(
-            self.test_application_prefix,
+            self.application_prefix,
             plugin_mapping)
 
         workspace_path = os.path.join(
