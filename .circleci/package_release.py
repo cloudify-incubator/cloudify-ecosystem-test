baec74a54a566e780020b8d2b3d48d1d1ed2129a
earthmant <trammell@cloudify.co>
2020-04-01 19:16:42 +0300

    latest

--- .circleci/package_release.py
+++ .circleci/package_release.py
@@ -16,11 +16,14 @@ if __name__ == '__main__':
         release.create_release(
             version, version, "cloudify-ecosystem-tests-v{0}".format(version),
             commit)
-    latest_release = release.get_most_recent_release()
     if not release.get_release("latest"):
-        release.create_release("latest")
-    release.update_release(
-        "latest",
-        "The latest release is version {0}.".format(version),
-    )
+        release.create_release(
+            "latest", "latest", "cloudify-ecosystem-tests-v{0}".format(version),
+            commit)
+    else:
+        release.update_release(
+            "latest",
+            "cloudify-ecosystem-tests-v{0}".format(version),
+        )
+    latest_release = release.get_most_recent_release()
     release.update_latest_release_resources(latest_release)
