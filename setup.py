de9246ae7e704563cc42200bfeecb229c261f685
earthmant <trammell@cloudify.co>
2020-04-05 19:19:55 +0300

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-ecosystem-test',
-    version='2.2.2',
+    version='2.2.3',
     license='LICENSE',
     packages=[
         'ecosystem_tests',
