3f7f6bc73f278849036a5b66311c7674815bf632
earthmant <trammell@cloudify.co>
2020-04-19 14:12:07 +0300

    fix that

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-ecosystem-test',
-    version='2.2.31',
+    version='2.2.32',
     license='LICENSE',
     packages=[
         'ecosystem_tests',
