f9b38c06169ba711c33c6864c18a50d664e7c1f5
earthmant <trammell@cloudify.co>
2020-04-19 10:31:14 +0300

    try out sol2 with 2.2.24

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-ecosystem-test',
-    version='2.2.22',
+    version='2.2.24',
     license='LICENSE',
     packages=[
         'ecosystem_tests',
