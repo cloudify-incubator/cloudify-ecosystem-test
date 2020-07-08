acab10b72efaebe3fcac9f042b3f556d3babf315
AdarShaked <adar@cloudify.co>
2020-07-08 10:55:29 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-ecosystem-test',
-    version='2.2.65',
+    version='2.2.66',
     license='LICENSE',
     packages=[
         'ecosystem_tests',
