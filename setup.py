2b397946127c934a5f192cae9fe13a4541d2d063
earthmant <trammell@cloudify.co>
2020-07-27 22:29:21 +0300

    nighty bundle build

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-ecosystem-test',
-    version='2.2.71',
+    version='2.2.72',
     license='LICENSE',
     packages=[
         'ecosystem_tests',
