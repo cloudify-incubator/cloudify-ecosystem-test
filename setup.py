3dd3610aa46bcc49d71dbe2f770bdd870af727e6
AdarShaked <59121240+AdarShaked@users.noreply.github.com>
2020-11-16 16:45:06 +0200

    Rd 527 add helm to bundle (#66)
    * add helm to plugins to bundle list
    
    * bump version and update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-ecosystem-test',
-    version='2.2.98',
+    version='2.2.99',
     license='LICENSE',
     packages=[
         'ecosystem_tests',
