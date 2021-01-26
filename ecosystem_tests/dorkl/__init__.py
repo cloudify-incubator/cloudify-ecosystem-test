f1c1a0e551552dcb8b1e356072cb7fdf2d5c6301
AdarShaked <adar@cloudify.co>
2021-01-26 17:20:11 +0200

    change name

--- ecosystem_tests/dorkl/__init__.py
+++ ecosystem_tests/dorkl/__init__.py
@@ -2,10 +2,10 @@
 
 from ecosystem_tests.dorkl.commands import cloudify_exec
 
-from ecosystem_tests.dorkl.testing import (prepare_test,
-                                           prepare_test_dev,
-                                           basic_blueprint_test,
-                                           basic_blueprint_test_dev)
+from ecosystem_tests.dorkl.runners import (prepare_test,
+                                       prepare_test_dev,
+                                       basic_blueprint_test,
+                                       basic_blueprint_test_dev)
 from ecosystem_tests.dorkl.cloudify_api import (executions_start,
                                                 blueprints_upload,
                                                 blueprints_delete,
