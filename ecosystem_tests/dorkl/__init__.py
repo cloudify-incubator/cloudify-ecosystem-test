19194ad2bde2a4191e433f8614993055cc654c67
AdarShaked <adar@cloudify.co>
2021-01-26 17:22:26 +0200

    flake8

--- ecosystem_tests/dorkl/__init__.py
+++ ecosystem_tests/dorkl/__init__.py
@@ -3,9 +3,9 @@
 from ecosystem_tests.dorkl.commands import cloudify_exec
 
 from ecosystem_tests.dorkl.runners import (prepare_test,
-                                       prepare_test_dev,
-                                       basic_blueprint_test,
-                                       basic_blueprint_test_dev)
+                                           prepare_test_dev,
+                                           basic_blueprint_test,
+                                           basic_blueprint_test_dev)
 from ecosystem_tests.dorkl.cloudify_api import (executions_start,
                                                 blueprints_upload,
                                                 blueprints_delete,
