68eaea40f424bd0726c9e9f3fb7625b4778adc33
EarthmanT <EarthmanT@users.noreply.github.com>
2021-08-19 09:32:05 -0400

    Update validate_plugin_version.py

--- ecosystem_tests/ecosystem_tests_cli/commands/validate_plugin_version.py
+++ ecosystem_tests/ecosystem_tests_cli/commands/validate_plugin_version.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import validation
+from ecosystem_cicd_tools import validations
 
 from ...dorkl.runners import blueprint_validate
 from ...ecosystem_tests_cli import ecosystem_tests
