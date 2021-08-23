bbc60612238ab130f04aa3cb92d819c150ca364f
nely <nely@cloudify.co>
2021-08-23 11:06:09 +0300

    add test test_validate_default_values

---
+++ ecosystem_tests/ecosystem_tests_cli/tests/commands/test_validate_plugin_version.py
@@ -0,0 +1,20 @@
+from mock import patch
+
+from ..commands import BaseCliCommandTest
+from ...commands.validate_plugin_version import validate_plugin_version
+from ...constants import DEFAULT_DIRECTORY_PATH
+
+
+class ValidatePluginVersionTest(BaseCliCommandTest):
+
+    @patch('ecosystem_tests.ecosystem_tests_cli.utilities.id_generator')
+    @patch('ecosystem_tests.ecosystem_tests_cli.commands'
+           '.validate_plugin_version.validate_plugin_version')
+    def test_validate_default_values(self,
+                                     mock_validate_plugin_version,
+                                     mock_id_generator):
+        mock_id_generator.return_value = self.test_id
+        self.runner.invoke(validate_plugin_version, [])
+        mock_validate_plugin_version.assert_called_once_with(
+            blueprint_file_name=DEFAULT_DIRECTORY_PATH,
+            blueprint_id=self.test_id)
