9af3ee9731906100b6da20b664f947b8b2125a1d
EarthmanT <trammell@cloudify.co>
2021-09-19 09:32:21 -0400

    fix release

--- ecosystem_tests/ecosystem_tests_cli/commands/package_release.py
+++ ecosystem_tests/ecosystem_tests_cli/commands/package_release.py
@@ -13,10 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from os import path, pardir
+import os
 
+from ...ecosystem_tests_cli import ecosystem_tests
+from ecosystem_cicd_tools.validations import get_plugin_yaml_version
 from ecosystem_cicd_tools.release import (
     plugin_release_with_latest, find_version)
-from ...ecosystem_tests_cli import ecosystem_tests
 
 
 @ecosystem_tests.command(name='package-release',
@@ -24,5 +26,9 @@ from ...ecosystem_tests_cli import ecosystem_tests
 @ecosystem_tests.options.directory
 @ecosystem_tests.options.name
 def package_release(directory, name):
-    setup_py = directory + 'setup.py'
-    plugin_release_with_latest(name, find_version(setup_py))
+    try:
+        setup_py = os.path.join(directory, 'setup.py')
+        plugin_release_with_latest(name, find_version(setup_py))
+    except (RuntimeError, FileNotFoundError):
+        plugin_yaml = os.path.join(directory, 'plugin.yaml')
+        plugin_release_with_latest(name, get_plugin_yaml_version(plugin_yaml))
