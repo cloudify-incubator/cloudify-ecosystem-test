e0efb364ab62b5e65815444ce26670c6566496d8
EarthmanT <trammell@cloudify.co>
2021-12-12 20:06:29 -0500

    one last fix

--- ecosystem_cicd_tools/tests/test_package.py
+++ ecosystem_cicd_tools/tests/test_package.py
@@ -35,7 +35,7 @@ class TestPackaging(unittest.TestCase):
             },
         ]
         plugin_dict = {
-            'link': 'foo',
+            'link': 'https:/foo/taco/izam',
             'version': 'wgn',
             'wagons': wagons_list
         }
@@ -56,7 +56,7 @@ class TestPackaging(unittest.TestCase):
                 'url': 'old-redhat-maipo.wgn',
             },
         ]
-        update_assets_in_plugin_dict(plugin_dict, assets)
+        update_assets_in_plugin_dict(plugin_dict, assets, 'wgn')
         print(plugin_dict['wagons'])
         print(expected)
         self.assertListEqual(plugin_dict['wagons'], sorted(
