963e6df0c524e433e2239e2927370499170b2f6e
EarthmanT <EarthmanT@users.noreply.github.com>
2023-03-29 13:37:17 +0300

    fix hamburgers

--- ecosystem_cicd_tools/validations.py
+++ ecosystem_cicd_tools/validations.py
@@ -19,6 +19,7 @@ from .github_stuff import (
     get_pull_requests,
     find_pull_request_numbers,
     check_if_label_in_pr_labels)
+from .new_cicd.github import with_github_client
 
 VERSION_EXAMPLE = """
 version_file = open(os.path.join(package_root_dir, 'VERSION'))
@@ -39,10 +40,18 @@ def does_protected_branch_have_build_source(pull_request):
         sys.exit(1)
 
 
-def validate_pulls(repo_name, branch_name=None):
-    client = get_client()
-    repo = client.get_repo(repo_name)
-    pulls = repo.get_pulls()
+@with_github_client
+def validate_pulls(repo_name=None,
+                   branch_name=None,
+                   github_client=None,
+                   repository=None,
+                   **kwargs):
+    if repo_name:
+        repo = github_client.get_repo(
+            '{}/{}'.format(kwargs.get('organization_name'), repo_name))
+        pulls = repo.get_pulls()
+    else:
+        pulls = repository.get_pulls()
     for pull in pulls:
         if pull.head.ref == branch_name:
             does_protected_branch_have_build_source(pull)
