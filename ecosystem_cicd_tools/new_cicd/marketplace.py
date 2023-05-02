83f13e042c4122bfd7c6b75fc30249a3f5eb9fea
EarthmanT <EarthmanT@users.noreply.github.com>
2023-05-02 11:59:08 -0400

    support revert via yank

--- ecosystem_cicd_tools/new_cicd/marketplace.py
+++ ecosystem_cicd_tools/new_cicd/marketplace.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 
 import json
-import logging
 
 import requests
 import urllib.request
@@ -47,18 +46,22 @@ def call_plugins_webhook(plugin_name, plugin_version, github_user):
 
 
 def get_plugin_id(plugin_name):
+    logger.info('Getting plugin ID for name: {}'.format(plugin_name))
     url_plugin_id = 'https://marketplace.cloudify.co/plugins?name={}'.format(
         plugin_name)
     json_resp = get_json(url_plugin_id)
+    logger.info('Got plugin ID response: {}'.format(json_resp))
     if 'items' in json_resp:
         if len(json_resp['items']) == 1:
             return json_resp['items'][0]['id']
 
 
 def get_plugin_versions(plugin_id):
+    logger.info('Getting plugin versions for ID: {}'.format(plugin_id))
     url_plugin_version = 'https://marketplace.cloudify.co/' \
                          'plugins/{}/versions?'.format(plugin_id)
     json_resp = get_json(url_plugin_version)
+    logger.info('Got plugin version response: {}'.format(json_resp))
     if 'items' in json_resp:
         versions = [item['version'] for item in json_resp['items']]
         return sorted(versions, key=lambda x: version_parse(x))
@@ -66,6 +69,8 @@ def get_plugin_versions(plugin_id):
 
 
 def get_node_types_for_plugin_version(plugin_name, plugin_version):
+    logger.info('Getting node types for {}:{}'.format(
+        plugin_name, plugin_version))
     url = 'https://marketplace.cloudify.co/node-types?' \
           '&plugin_name={}' \
           '&plugin_version={}'.format(plugin_name, plugin_version)
@@ -77,6 +82,8 @@ def get_node_types_for_plugin_version(plugin_name, plugin_version):
 
 
 def list_versions(plugin_id):
+    logger.info('Getting plugin versions for {}'.format(
+        plugin_id))
     url = f'{URL_MARKETPLACE}/plugins/{plugin_id}/versions'
     json_resp = get_json_from_marketplace(url)
     if 'items' in json_resp:
@@ -85,7 +92,7 @@ def list_versions(plugin_id):
     return []
 
 
-def get_json_from_marketplace(url):
+def get_json_from_marketplace(url, log_response=False):
     logger.info('get_json_from_marketplace request {}.'.format(url))
     try:
         resp = urllib.request.urlopen(url)
@@ -93,7 +100,8 @@ def get_json_from_marketplace(url):
         return {}
     body = resp.read()
     result = json.loads(body)
-    logger.info('get_json_from_marketplace response {}.'.format(result))
+    if log_response:
+        logger.info('get_json_from_marketplace response {}.'.format(result))
     return result
 
 
@@ -104,10 +112,10 @@ def get_plugin_release_spec_from_marketplace(plugin_id, plugin_version):
 
 
 def get_assets(repository, version):
+    logger.info('Getting Assets: {} {}'.format(repository, version))
     plugin_id = get_plugin_id(repository.name)
-    logger.info('PLUGIN IN {}.'.format(plugin_id))
     items = get_plugin_release_spec_from_marketplace(plugin_id, version)
-    logger.info('Items: {}.'.format(items))
+    logger.info('Got assets: {}'.format(items))
     if not items:
         return items
     assets_list_marketplace = []
@@ -120,3 +128,32 @@ def get_assets(repository, version):
     for wagon in wagon_urls:
         assets_list_marketplace.append(wagon['url'])
     return assets_list_marketplace
+
+
+def get_node_types_diff(plugin_id, old, new):
+    """Get the node types diff between two versions of
+    a plugin. The new_node_types dict will return node
+    types that are not present in the old version.
+    """
+    node_types_diff = {}
+    old_node_types = get_node_types_for_plugin_version(
+        plugin_id, old)
+    new_node_types = get_node_types_for_plugin_version(
+        plugin_id, new)
+    for key, value in new_node_types.items():
+        if key not in old_node_types:
+            node_types_diff[key] = value
+    return node_types_diff
+
+
+def delete_plugin_version(plugin_id, plugin_version):
+    url = 'https://marketplace.cloudify.co/plugins/{}/{}'.format(
+        plugin_id, plugin_version)
+    return get_json(url=url, method='DELETE')
+
+
+def delete_node_type(node_type_id):
+    url = 'https://marketplace.cloudify.co/node-types/{}'.format(
+        node_type_id)
+    return get_json(url=url, method='DELETE')
+
